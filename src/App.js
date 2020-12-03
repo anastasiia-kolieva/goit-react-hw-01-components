63d4d6e8a6c5d51920b03fbb6b94d492cec08d4b
anastasiia-kolieva <nastya1110@gmail.com>
2020-12-03 10:20:17 +0200

    made folder data for json files

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import FriendList from './components/FriendList/FriendList';
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-import friends from './friends.json';
-import user from './user.json';
-import statisticalData from './statistical-data.json';
-import transactions from './transactions';
+import friends from './data/friends.json';
+import user from './data/user.json';
+import statisticalData from './data/statistical-data.json';
+import transactions from './data/transactions';
 
 export default function App() {
   return (
